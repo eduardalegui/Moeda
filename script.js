216eb8be26dfaf7e5064c994f9b20759d998c41f
Henrique Collatto <chcollatto@gmail.com>
2025-03-18 16:02:29 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const apiKey = "37a26a6057489965c5ad1d60"
+const apiKey = "37a26a6057489965c5ad1d60";
+
+async function getRates() {
+    alert("oi");
+    let data = await fetch("https://v6.exchangerate-api.com/v6/37a26a6057489965c5ad1d60/latest/USD");
+        if(!data.ok){
+            return
+        }
+    console.log(data.json());
+
+    
+
+}
